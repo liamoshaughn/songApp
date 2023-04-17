5e5f8677cc87d8ee5903c5c2979b15c30b0495e3
Liam Stoldt <liamstoldt@yahoo.com>
2023-04-17 21:10:51 +0930

    initial setup for home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Typography, Container } from "@mui/material";
 import DevelopButtons from "../components/DevelopButtons";
 import Cookies from "universal-cookie";
 import { addToken, addUser, useStore } from "../store/store";
 import { callback, getUser } from "../services/api";
+import SongSearch from "../components/UserAdd/SongSearch";
 
 async function getUserProfile(token) {
   const request = await getUser(token);
@@ -19,6 +20,11 @@ function Home() {
   const cookies = new Cookies();
   const store = useStore();
 
+  async function addTracks(song) {
+    const response = "add track I guess"
+    console.log(response);
+  }
+
   const location = useLocation();
   useEffect(() => {
     if (location.pathname == "/auth/callback") {
@@ -29,31 +35,10 @@ function Home() {
   }, []);
 
   return (
-    <div className="App">
-      <DevelopButtons />
-      <Grid spacing={3} container direction="row" justifyContent="center" alignItems="center" sx={{ marginY: "auto" }}>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={() => {
-              navigate("/list");
-            }}
-          >
-            Go to List
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={() => {
-              navigate("/spotifyAdd");
-            }}
-          >
-            Go to Spotify Add
-          </Button>
-        </Grid>
-      </Grid>
-    </div>
+    <Container>
+      <Typography variant="h4">Request songs for PARTY NAME</Typography>
+      <SongSearch addTracks={(song) => addTracks(song)} />
+    </Container>
   );
 }
 
