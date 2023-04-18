cfef492e0b88db304d83a73ed59ba9440dff2b87
Liam O'Shaughnessy <liam@jpmedia.com.au>
2023-04-18 20:09:59 +0930

    Added in send message

--- src/components/DevelopButtons.js
+++ src/components/DevelopButtons.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import XMLConvert from "../components/helpers/XMLConvert";
 import { batchSearch } from "../components/helpers/batchSearch";
+import { ClientComponent } from "../components/helpers/sendData";
 import Spotify from "../components/helpers/Spotify";
 
 import { addToken, addUser, useStore } from "../store/store";
@@ -13,6 +14,7 @@ export default function DevelopButtons() {
       <Spotify />
       <button onClick={() => batchSearch(store.accessToken)}>Batch Search</button>
       <XMLConvert />
+      <ClientComponent />
     </div>
   );
 }
