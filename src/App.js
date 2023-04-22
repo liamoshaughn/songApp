7c82ceae597fabccc7c4b0438d93f36cd98a55cb
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2023-04-22 18:14:41 +0930

    Ui Changes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Header from "./components/Header";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/callback" element={<Home />}></Route>
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 import { Grid, Button, Typography, Container } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 import { sendMessage } from "../services/api";
 import SongSearch from "../components/UserAdd/SongSearch";
 
 function Home() {
+  const theme = useTheme();
+  console.log(theme);
   return (
-    <Container>
-      <Typography variant="h4">Request songs for PARTY NAME</Typography>
+    <Container
+      sx={{
+        display: "flex",
+        flexFlow: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="h4">Request songs for Jacobs 21st</Typography>
+      <Typography variant="subtitle1">Press the "+" once to add a song</Typography>
       <SongSearch addTracks={(song) => sendMessage(song.id)} />
     </Container>
   );
