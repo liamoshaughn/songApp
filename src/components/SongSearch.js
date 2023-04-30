3909e567947e2df074ccb8f812a65a13ae736672
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2023-04-30 11:55:16 +0930

    Dont actually know hat changed here, might be linting

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -1,13 +1,13 @@
 //Lists songs and playlists uploaded by user
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { Button, Typography, Container, Grid, TextField, InputAdornment } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { searchBarSong } from "../services/api";
-import { DataGrid, GridToolbarQuickFilter } from "@mui/x-data-grid";
-import { useStore } from "../store/store";
-import { sendMessage } from "../services/api";
+import { Button, Typography, Container, Grid, TextField, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { searchBarSong } from '../services/api';
+import { DataGrid, GridToolbarQuickFilter } from '@mui/x-data-grid';
+import { useStore } from '../store/store';
+import { sendMessage } from '../services/api';
 
 export default function SongSearch() {
   const store = useStore();
@@ -18,50 +18,49 @@ export default function SongSearch() {
   async function searchSong(song) {
     const response = await searchBarSong(song, store.accessToken);
     setRows(response.tracks.items);
-    var temp = Array(rows.length).fill("+");
+    var temp = Array(rows.length).fill('+');
     setButt(temp);
-    temp = Array(rows.length).fill("lightblue");
-    setButtcol(temp)
+    temp = Array(rows.length).fill('lightblue');
+    setButtcol(temp);
   }
 
   function buttonCheck(index, test) {
     var temp;
     if (test) {
       temp = [...buttcol];
-      temp[index] = "lightgreen";
+      temp[index] = 'lightgreen';
       setButtcol(temp);
       temp = [...butt];
-      temp[index] = "✓";
+      temp[index] = '✓';
       setButt(temp);
     } else {
       temp = [...buttcol];
-      temp[index] = "lightcoral";
+      temp[index] = 'lightcoral';
       setButtcol(temp);
       temp = [...butt];
-      temp[index] = "↺";
+      temp[index] = '↺';
       setButt(temp);
     }
   }
 
   async function addTracks(song, index) {
-    if (butt[index] === "✓") {
-      console.log("This button has been already pressed")
-    }
-    else {
+    if (butt[index] === '✓') {
+      console.log('This button has been already pressed');
+    } else {
       //add login here
-      var response = await sendMessage(song);
-      console.log("printing status")
-      console.log(response.status)
+      var response = await sendMessage(song.id);
+      console.log('printing status');
+      console.log(response.status);
       if (response.status === 200) {
-        buttonCheck(index, 1)
+        buttonCheck(index, 1);
       } else {
-        buttonCheck(index, 0)
+        buttonCheck(index, 0);
       }
     }
   }
 
   return (
-    <Grid container spacing={2} maxWidth={"500px"} width={"75vw"}>
+    <Grid container spacing={2} maxWidth={'500px'} width={'75vw'}>
       <Grid item xs={12}>
         <TextField
           placeholder="Search for a song"
@@ -80,24 +79,29 @@ export default function SongSearch() {
         return (
           <>
             <Grid item xs={3}>
-              <img style={{ maxWidth: "100%" }} alt="Album Cover" src={song.album.images[1].url} />
+              <img style={{ maxWidth: '100%' }} alt="Album Cover" src={song.album.images[1].url} />
             </Grid>
             <Grid
               item
               xs={7}
-              sx={{ display: "flex", justifyContent: "center", flexDirection: "column", textAlign: "left" }}
+              sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', textAlign: 'left' }}
             >
-              <Typography sx={{ typography: { sm: "body1", xs: "body2" } }}>{song.name}</Typography>
-              <Typography sx={{ typography: { sm: "body1", xs: "body2" } }}>
+              <Typography sx={{ typography: { sm: 'body1', xs: 'body2' } }}>{song.name}</Typography>
+              <Typography sx={{ typography: { sm: 'body1', xs: 'body2' } }}>
                 {song.artists
                   .map((artist) => {
                     return artist.name;
                   })
-                  .join(", ")}
+                  .join(', ')}
               </Typography>
             </Grid>
-            <Grid item xs={2} sx={{ display: "flex", alignItems: "center" }}>
-              <Button variant="contained" sx={{ height: "40px", backgroundColor: buttcol[index], color: "black" }} size="small" onClick={() => addTracks(song, index)}>
+            <Grid item xs={2} sx={{ display: 'flex', alignItems: 'center' }}>
+              <Button
+                variant="contained"
+                sx={{ height: '40px', backgroundColor: buttcol[index], color: 'black' }}
+                size="small"
+                onClick={() => addTracks(song, index)}
+              >
                 {butt[index]}
               </Button>
             </Grid>
