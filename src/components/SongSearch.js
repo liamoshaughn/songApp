61f41dd3743184289f06f8a73557fe50541afaf9
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2023-05-03 15:13:39 +0930

    Added song tile component and begining of client feedback

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -2,69 +2,29 @@
 
 import { useEffect, useState } from 'react';
 
-import { Button, Typography, Container, Grid, TextField, InputAdornment, useTheme } from '@mui/material';
+import { Typography, Grid, TextField, InputAdornment, useTheme } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { searchBarSong } from '../services/api';
-import { DataGrid, GridToolbarQuickFilter } from '@mui/x-data-grid';
 import { useStore } from '../store/store';
-import { usePostMessageMutation } from '../services/api';
+import SongTile from './SongTile';
 
 export default function SongSearch(props) {
   const store = useStore();
   const theme = useTheme();
-const sendMessage = usePostMessageMutation();
-
 
   const [rows, setRows] = useState([]);
-  const [butt, setButt] = useState([]);
-  const [buttcol, setButtcol] = useState([]);
 
   async function searchSong(song) {
     if (props.enable) {
       const response = await searchBarSong(song, store.accessToken);
+
       setRows(response.tracks.items);
-      var temp = Array(rows.length).fill('+');
-      setButt(temp);
-      temp = Array(rows.length).fill('lightblue');
-      setButtcol(temp);
-    }
-  }
-
-  function buttonCheck(index, test) {
-    var temp;
-    if (test) {
-      temp = [...buttcol];
-      temp[index] = 'lightgreen';
-      setButtcol(temp);
-      temp = [...butt];
-      temp[index] = '✓';
-      setButt(temp);
-    } else {
-      temp = [...buttcol];
-      temp[index] = 'lightcoral';
-      setButtcol(temp);
-      temp = [...butt];
-      temp[index] = '↺';
-      setButt(temp);
-    }
-  }
-
-  async function addTracks(song, index) {
-    if (butt[index] === '✓') {
-      console.log('This button has been already pressed');
-    } else {
-      //add login here
-      sendMessage.mutate({ message: song.id, name: props.name });
-
-
     }
   }
 
   return (
     <Grid container spacing={2} maxWidth={'500px'} width={'75vw'}>
       <Grid item xs={12}>
-         {sendMessage.isLoading && "hello world"}
-         
         <TextField
           fullWidth
           placeholder="Search for a song"
@@ -77,46 +37,14 @@ const sendMessage = usePostMessageMutation();
             ),
           }}
         ></TextField>
-       
         {!props.enable && (
           <Typography variant="subtitle2" color={theme.palette.error.main}>
             Please enter name before searching
           </Typography>
         )}
       </Grid>
-
       {rows.map((song, index) => {
-        return (
-          <>
-            <Grid item xs={3}>
-              <img style={{ maxWidth: '100%' }} alt="Album Cover" src={song.album.images[1].url} />
-            </Grid>
-            <Grid
-              item
-              xs={7}
-              sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', textAlign: 'left' }}
-            >
-              <Typography sx={{ typography: { sm: 'body1', xs: 'body2' } }}>{song.name}</Typography>
-              <Typography sx={{ typography: { sm: 'body1', xs: 'body2' } }}>
-                {song.artists
-                  .map((artist) => {
-                    return artist.name;
-                  })
-                  .join(', ')}
-              </Typography>
-            </Grid>
-            <Grid item xs={2} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Button
-                variant="contained"
-                sx={{ height: '40px', backgroundColor: buttcol[index], color: 'black' }}
-                size="small"
-                onClick={() => addTracks(song, index)}
-              >
-                {butt[index]}
-              </Button>
-            </Grid>
-          </>
-        );
+        return <SongTile key={index} song={song} />;
       })}
     </Grid>
   );
---
+++ src/components/SongTile.js
@@ -0,0 +1,80 @@
+import { Button, Typography, Grid, Box, useTheme } from '@mui/material';
+import React from 'react';
+import { addToQueue } from '../services/api';
+import { usePostMessageMutation } from '../services/api';
+import { useStore } from '../store/store';
+export default function SongTile({ song }) {
+  const sendMessage = usePostMessageMutation();
+  const store = useStore();
+  const theme = useTheme();
+
+  function handleSend() {
+    sendMessage.mutate({ message: song.id, name: song.name });
+  }
+
+  return (
+    <Grid sx={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }} item xs={12}>
+      {sendMessage.isSuccess && (
+        <Box
+          sx={{
+            width: 500 * 0.9 + 'px',
+            height: 120 * 0.9 + 'px',
+            position: 'absolute',
+            backgroundColor: theme.palette.success.main,
+            zIndex: '100',
+            opacity: '0.97',
+            boxShadow: '0 0 10px 10px ' + theme.palette.success.main,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            Song Sent
+          </Typography>
+        </Box>
+      )}
+      <Box
+        sx={{
+          width: '500px',
+          height: '120px',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img style={{ maxWidth: '100px' }} alt="Album Cover" src={song.album.images[1].url} />
+        <div style={{ paddingLeft: '15px', width: '220px' }}>
+          <Box>
+            <Typography sx={{ fontWeight: 'bold' }}>
+              {new Date(song.album.release_date).toLocaleString('en-AU', {
+                year: 'numeric',
+              })}
+            </Typography>
+          </Box>
+
+          <Typography sx={{ typography: { sm: 'body2', xs: 'body2' } }}>{song.name}</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>
+            {song.artists
+              .map((artist) => {
+                return artist.name;
+              })
+              .join(', ')}
+          </Typography>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            sx={{ height: '30px', margin: '10px' }}
+            onClick={() => {
+              handleSend();
+            }}
+          >
+            Request
+          </Button>
+        </div>
+      </Box>
+    </Grid>
+  );
+}
